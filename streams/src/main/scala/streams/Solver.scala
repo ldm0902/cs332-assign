ff3bc90e82f6ed6d6b660a70c32c5f365df094f2
ldm0902 <ldm0902@postech.ac.kr>
2024-10-12 20:15:12 +0900

    implement finished

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -62,18 +62,29 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = initial match{
+    case Stream.Empty => Stream.Empty
+    case x #:: tail => {
+      val newExplored = explored+x._1
+      val newNeighbors = newNeighborsOnly(neighborsWithHistory(x._1, x._2), newExplored)
+
+      newNeighbors #::: from(tail #::: newNeighbors,newExplored)
+    }
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] =  from(Stream((startBlock,Nil)),Nil.toSet)
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = {
+    for (path <- pathsFromStart
+         if done(path._1)) yield path
+  }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -83,5 +94,7 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = {
+    if(pathsToGoal.isEmpty) Nil else pathsToGoal.head._2
+  }
 }
